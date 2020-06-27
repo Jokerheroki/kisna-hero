285df608eaceb0eb893134a38134aec7a7fde69c
Jokerheroki <67479462+Jokerheroki@users.noreply.github.com>
2020-06-27 21:05:20 +0530

    Update and rename zeroboy.py to kisnaverma.py

--- kisnaverma.py
+++ kisnaverma.py
@@ -49,16 +49,16 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
+|__(~)    •||•THE - KISNA -VERMA------•◈•
+|__\~~) •||•HERO - OF - LOST - WORLD---------------•◈•
 |__(-----\  •◈•------BLACK-TIGER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92mkisna verma
+\033[1;96mInstagram \033[1;93m: \033[1;none 
+\033[1;96mFacebook  \033[1;93m: \033[1; non
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
 \033[1;91m======================================="""
 
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92mkisna verma
+\033[1;96mInstagram \033[1;93m: \033[1;92mnone
+\033[1;96mFacebook  \033[1;93m: \033[1;92m none
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "kisna"
+CorrectPassword = "kisna"
 
 loop = 'true'
 while (loop == 'true'):
@@ -103,7 +103,7 @@ while (loop == 'true'):
             os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.Youtube.com/https://www.youtube.com/channel/UCkuxmyzvVmxsbwGy5A4ilmQ')
 
 def login():
 	os.system('clear')
